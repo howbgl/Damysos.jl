c9210ea3ce96f42d442d2c3743b07db095dcb5b0
Damysos <wolfgang.hogger@ur.de>
2023-12-13 11:14:09 +0100

    Split observables in seperate files

---
+++ src/observables/Observables.jl
@@ -0,0 +1,60 @@
+import Base.empty,Base.zero
+
+export Observable,getnames_obs,zero!,resize
+
+sig(x)         = 0.5*(1.0+tanh(x/2.0)) # = logistic function 1/(1+e^(-t)) 
+
+include("Velocity.jl")
+include("Occupation.jl")
+
+function getmovingbz(sim::Simulation{T}) where {T<:Real}
+    p              = getparams(sim)
+    return getmovingbz(sim,p.kxsamples)
+end
+
+function getmovingbz(sim::Simulation{T},kxsamples::AbstractVector{T}) where {T<:Real}
+    p              = getparams(sim)
+    nkx            = length(kxsamples)
+    ax             = get_vecpotx(sim.drivingfield)
+    ay             = get_vecpoty(sim.drivingfield)
+    moving_bz      = zeros(T,nkx,p.nt)
+    bzmask1d(kx)   = sig((kx-p.bz[1])/(2*p.dkx)) * sig((p.bz[2]-kx)/(2*p.dkx))
+
+    for i in 1:p.nt
+        moving_bz[:,i] .= bzmask1d.(kxsamples .- ax(p.tsamples[i]))
+    end
+    return moving_bz    
+end
+
+function integrateobs!(
+    observables::Vector{Vector{Observable{T}}},
+    observables_dest::Vector{Observable{T}},
+    vertices::AbstractVector{T}) where {T<:Real}
+
+    for (i,odest) in enumerate(observables_dest) 
+        integrateobs!([o[i] for o in observables],odest,collect(vertices))
+    end    
+end
+
+function integrateobs_threaded!(
+    observables::Vector{Vector{Observable{T}}},
+    observables_dest::Vector{Observable{T}},
+    vertices::AbstractVector{T}) where {T<:Real}
+
+    for (i,odest) in enumerate(observables_dest) 
+        integrateobs_threaded!([o[i] for o in observables],odest,collect(vertices))
+    end    
+end
+
+function integrateobs_kxbatch_add!(
+    sim::Simulation{T},
+    sol,
+    kxsamples::AbstractVector{T},
+    ky::T,
+    moving_bz::AbstractMatrix{T},
+    obsfuncs) where {T<:Real}
+
+    for (o,funcs) in zip(sim.observables,obsfuncs)
+        integrateobs_kxbatch_add!(sim,o,sol,kxsamples,ky,moving_bz,funcs)
+    end        
+end
---
+++ src/observables/Occupation.jl
@@ -0,0 +1,98 @@
+export Occupation
+struct Occupation{T<:Real} <: Observable{T}
+    cbocc::Vector{T}
+end
+function Occupation(h::Hamiltonian{T}) where {T<:Real}
+    return Occupation(Vector{T}(undef,0))
+end
+function Occupation(p::NumericalParameters{T}) where {T<:Real}
+    return Occupation(zeros(T,getparams(p).nt))
+end
+
+function resize(o::Occupation{T},p::NumericalParameters{T}) where {T<:Real}
+    return Occupation(p)
+end
+
+function empty(o::Occupation)
+    return Occupation(o)
+end
+
+getnames_obs(occ::Occupation{T}) where {T<:Real} = ["cbocc", "cbocck"]
+getparams(occ::Occupation{T}) where {T<:Real}    = getnames_obs(occ)
+arekresolved(occ::Occupation{T}) where {T<:Real} = [false, true]
+
+@inline function addto!(o::Occupation{T},ototal::Occupation{T}) where {T<:Real}
+    ototal.cbocc .= ototal.cbocc .+ o.cbocc
+end
+
+@inline function copyto!(odest::Occupation,osrc::Occupation)
+    odest.cbocc .= osrc.cbocc
+end
+
+@inline function normalize!(o::Occupation{T},norm::T) where {T<:Real}
+    o.cbocc ./= norm
+end
+
+function zero(o::Occupation{T}) where {T<:Real}
+    cbocc = zero(o.cbocc)
+    return Occupation(cbocc)
+end
+
+function getfuncs(sim::Simulation,o::Occupation)
+    return []
+end
+
+
+function integrateobs_kxbatch_add!(
+    sim::Simulation{T},
+    o::Occupation{T},
+    sol,
+    kxsamples::AbstractVector{T},
+    ky::T,
+    moving_bz::AbstractMatrix{T},
+    funcs) where {T<:Real}
+
+    ts    = getparams(sim).tsamples
+    nkx   = length(kxsamples)
+
+    for i in eachindex(ts)
+        ρcc             = @view sol[1:nkx,i]
+        o.cbocc[i]      += trapz(kxsamples,moving_bz[:,i] .* real.(ρcc))
+    end
+    return o
+end
+
+
+function integrateobs_kxbatch!(sim::Simulation{T},o::Occupation{T},sol,ky::T,
+                    moving_bz::Array{T}) where {T<:Real}
+
+    p           = getparams(sim)
+    nkx_bz      = Int(cld(2*p.bz[2],p.dkx))
+
+    occ_k_itp   = zeros(T,nkx_bz,length(sol.t))
+    occ_k       = zeros(T,p.nkx,length(sol.t))
+    occ         = zeros(T,length(sol.t))
+    
+    calcobs_k1d!(sim,o,sol,occ_k,occ_k_itp)
+
+    occ         = trapz((p.kxsamples,:),occ_k .* moving_bz)
+
+    return Occupation(occ)
+end
+
+function integrateobs(
+    occs::Vector{Occupation{T}},
+    vertices::Vector{T}) where {T<:Real}
+
+    cbocc  = trapz((:,hcat(vertices)),hcat([o.cbocc for o in occs]...))
+    return Occupation(cbocc)
+end
+
+
+function integrateobs!(
+    occs::Vector{Occupation{T}},
+    odest::Occupation{T},
+    vertices::Vector{T}) where {T<:Real}
+
+    odest.cbocc .= trapz((:,hcat(vertices)),hcat([o.cbocc for o in occs]...))
+end

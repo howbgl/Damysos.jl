7339411bef4037448db35d64f6d14eac830b183a
Wolfgang Hogger <wolfgang.hogger@ur.de>
2024-06-05 13:59:48 +0200

    Updated scripts

--- scripts/nhr_fau/zeta-M-sweep1_dt.jl
+++ scripts/nhr_fau/zeta-M-sweep1_dt.jl
@@ -5,9 +5,10 @@ function make_teelogger(logging_path::AbstractString, name::AbstractString)
 	ensurepath(logging_path)
 	info_filelogger = FileLogger(joinpath(logging_path, name) * "_$(now()).log")
 	info_logger     = MinLevelLogger(info_filelogger, Logging.Info)
+	tlogger 		= MinLevelLogger(TerminalLogger(),Logging.Info)
 	all_filelogger  = FileLogger(joinpath(logging_path, name) * "_$(now())_debug.log")
 
-	return TeeLogger(TerminalLogger(stderr,Logging.Info), info_logger, all_filelogger)
+	return TeeLogger(tlogger, info_logger, all_filelogger)
 end
 
 
@@ -19,7 +20,7 @@ function make_simulation(M = 0.5, ζ = 4.0, datapath = "~/data", plotpath = data
 	# emax      = u"0.5MV/cm"
 	e = uconvert(u"C", 1u"eV" / 1u"V")
 	ω = 2π*freq
-	m = M /(2ħ * ω)
+	m = M * ħ * ω / 2
 
 	σ = uconvert(u"fs", 1.5 / freq)
 	emax = uconvert(u"MV/cm", ζ * ħ * ω^2 / (2vf * e))
@@ -28,7 +29,6 @@ function make_simulation(M = 0.5, ζ = 4.0, datapath = "~/data", plotpath = data
 	t2 = Inf * u"s"
 	t1 = Inf * u"s"
 
-	us   = scaledriving_frequency(freq, vf)
 	h    = GappedDirac(energyscaled(m, us))
 	l    = TwoBandDephasingLiouvillian(h, timescaled(t1, us), timescaled(t2, us))
 	df   = GaussianAPulse(us, σ, freq, emax)
@@ -44,34 +44,39 @@ function make_simulation(M = 0.5, ζ = 4.0, datapath = "~/data", plotpath = data
 	pars = NumericalParams2d(dkx, dky, kxmax, kymax, dt, -5df.σ)
 	obs  = [Velocity(pars), Occupation(pars)]
 
-	id = "zeta-M-sweep1_dt"
+	id = "M=$(M)_zeta=$(ζ)_dt"
 
 	return Simulation(l, df, pars, obs, us, id, datapath, plotpath)
 end
 
 function make_dt_test(sim::Simulation,altpath::String=pwd())
 	solver = LinearCUDA(10_000)
-	method = PowerLawTest(:dt,0,5)
+	method = PowerLawTest(:dt,0.5)
 	atolgoal = 1e-12
 	rtolgoal = 1e-5
-	maxtime = u"20minute"
-	maxiter = 64
+	maxtime = u"5minute"
+	maxiter = 32 # min dt = 0.01/2^31 ≈ 5e-12
 	return ConvergenceTest(sim,solver,method,atolgoal,rtolgoal,maxtime,maxiter;
 		altpath = altpath)
 end
 
-const path = joinpath(ENV["WORK"], "zeta-M-sweep1_dt")
-const altpath = joinpath(ENV["HPCVAULT"], "zeta-M-sweep1_dt")
-const sim = make_simulation(0.8,2.0,path)
-const test = make_dt_test(sim,altpath)
+const Ms = exp10.(LinRange(-1,1,32))
+const ζs = exp10.(LinRange(-1,1,32))
+const id = parse(Int,ENV["SLURM_ARRAY_TASK_ID"])
+const ζ = ζs[id]
 
-const logger = make_teelogger(sim.plotpath, sim.id*"_slurmid="*ENV["SLURM_JOB_ID"])
+path(ζ) = joinpath(ENV["WORK"], "zeta-M-sweep/dt/zeta=$(ζ)")
+altpath(ζ) = joinpath(ENV["HPCVAULT"], "zeta-M-sweep/dt/zeta=$(ζ)")
+const sims = [make_simulation(m,ζ,path(ζ)) for m in Ms]
+const tests = [make_dt_test(s,altpath(ζ)) for (s,m) in zip(sims,Ms)]
+
+const logger = make_teelogger(
+	joinpath(ENV["WORK"],"zeta-M-sweep/dt"), 
+	"slurmid="*ENV["SLURM_ARRAY_TASK_ID"])
 
 global_logger(logger)
 @info "$(now())\nOn $(gethostname()):"
 
-result = run!(test)
-
-print(repr("text/plain",result))
+run!.(tests)
 
 @info "$(now()): calculation finished."

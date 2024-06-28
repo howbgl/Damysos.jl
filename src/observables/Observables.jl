af9e1e4a8a1902013f3c4f8da51dd99eb48caba6
Wolfgang Hogger <wolfgang.hogger@ur.de>
2024-06-28 16:22:07 +0200

    Fixed double definition due to last commit

--- src/observables/Observables.jl
+++ src/observables/Observables.jl
@@ -73,23 +73,6 @@ function getmovingbz(sim::Simulation{T},kxsamples::AbstractVector{T}) where {T<:
     return moving_bz    
 end
 
-
-function getbzbounds(df::GaussianAPulse,p::NumericalParams1d)
-    kxmax = p.kxmax
-    axmax = df.eE / df.ω
-    return (-kxmax + 1.3axmax,kxmax - 1.3axmax)
-end
-
-function getbzbounds(df::GaussianAPulse,p::NumericalParams2d)
-
-    amax = 1.3df.eE / df.ω
-    return (
-        -p.kxmax + cos(df.φ)*amax,
-        p.kxmax - cos(df.φ)*amax,
-        -p.kymax + sin(df.φ)*amax,
-        p.kymax - sin(df.φ)*amax)
-end
-
 function maximum_kdisplacement(df::DrivingField,ts::AbstractVector{<:Real})
     axmax = maximum(abs.(map(t -> vecpotx(df,t),ts)))
     aymax = maximum(abs.(map(t -> vecpoty(df,t),ts)))

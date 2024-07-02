ca8afaaba1ca80833b7efa5f41276b19c1a0c18d
Wolfgang Hogger <wolfgang.hogger@ur.de>
2024-07-02 11:01:53 +0200

    Added 1d tests and referencedata

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,18 +21,23 @@ function checkvelocity(sim::Simulation, solver::DamysosSolver, fns, vref::Veloci
 	return isapprox(v, vref, atol = atol, rtol = rtol)
 end
 
-function makectest(sim::Simulation, m::ConvergenceTestMethod, s::DamysosSolver;
+function makectest(sim::Simulation, m::ConvergenceTestMethod, s::Union{Nothing,DamysosSolver};
 	atol::Real = 1e-10,
 	rtol::Real = 1e-3)
-	return ConvergenceTest(sim, s, m, atol, rtol)
+	return isnothing(s) ? nothing : ConvergenceTest(sim, s, m, atol, rtol)
 end
 
 const sim1 = make_test_simulation1()
 const sim1_dt = make_test_simulation1(0.08, 1.0, 1.0, 175, 2)
 const sim1_kxmax = make_test_simulation1(0.01, 1.0, 1.0, 150, 2)
 
+const sim2 = make_test_simulation2()
+const sim2_dkx = make_test_simulation2(0.01,2.0)
+
 const linchunked = LinearChunked()
-const fns_linchunked = define_functions(sim1, linchunked)
+const fns1_linchunked = define_functions(sim1, linchunked)
+const fns2_linchunked = define_functions(sim2, linchunked)
+
 const solver_autovern7 = LinearChunked(
 	Damysos.default_kchunk_size(LinearChunked),
 	Damysos.choose_threaded_or_distributed(),
@@ -49,7 +54,8 @@ catch err
 	end
 end
 const lincuda = skipcuda ? nothing : LinearCUDA()
-const fns_lincuda = skipcuda ? nothing : define_functions(sim1, lincuda)
+const fns1_lincuda = skipcuda ? nothing : define_functions(sim1, lincuda)
+const fns2_lincuda = skipcuda ? nothing : define_functions(sim2, lincuda)
 
 
 const referencedata = DataFrame(CSV.File("referencedata.csv"))
@@ -60,6 +66,15 @@ const vref = Velocity(
 	referencedata.vy,
 	referencedata.vyintra,
 	referencedata.vyinter)
+
+const referencedata1d = DataFrame(CSV.File("referencedata1d.csv"))
+const vref1d = Velocity(
+	referencedata1d.vx,
+	referencedata1d.vxintra,
+	referencedata1d.vxinter,
+	referencedata1d.vy,
+	referencedata1d.vyintra,
+	referencedata1d.vyinter)
 
 const alldrivingfields = getall_drivingfields()
 const alldrivingfield_fns = getfield_functions.(alldrivingfields)
@@ -72,12 +87,12 @@ const alldrivingfield_fns = getfield_functions.(alldrivingfields)
 		end
 	end
 
-	@testset "Simulation 1" begin
+	@testset "Simulation 1 (2d)" begin
 		@testset "LinearCUDA" begin
-			@test checkvelocity(sim1, lincuda, fns_lincuda, vref) skip = skipcuda
+			@test checkvelocity(sim1, lincuda, fns1_lincuda, vref) skip = skipcuda
 		end
 		@testset "LinearChunked" begin
-			@test checkvelocity(sim1, linchunked, fns_linchunked, vref)
+			@test checkvelocity(sim1, linchunked, fns1_linchunked, vref)
 		end
 
 		@testset "AutoVern7" begin
@@ -85,15 +100,28 @@ const alldrivingfield_fns = getfield_functions.(alldrivingfields)
 		end
 	end
 
+	@testset "Simulation 2 (1d)" begin
+		@testset "LinearChunked" begin
+			@test checkvelocity(sim2, linchunked, fns2_linchunked, vref1d)
+		end
+		@testset "LinearCUDA" begin
+			@test checkvelocity(sim2, lincuda, fns2_lincuda, vref1d) skip = skipcuda
+		end
+	end
+
 	@testset "ConvergenceTest" begin
 		@testset "LinearChunked" begin
 			@testset "PowerLawTest (dt)" begin
 				ctest = makectest(sim1_dt, PowerLawTest(:dt, 0.5), linchunked)
-				@test successful_retcode(run!(ctest)) skip = skipcuda
+				@test successful_retcode(run!(ctest))
 			end
 			@testset "LinearTest (kxmax)" begin
 				ctest = makectest(sim1_kxmax, LinearTest(:kxmax, 10), linchunked)
-				@test successful_retcode(run!(ctest)) skip = skipcuda
+				@test successful_retcode(run!(ctest))
+			end
+			@testset "PowerLawTest (dkx)" begin
+				ctest = makectest(sim2_dkx, PowerLawTest(:dkx,0.5), linchunked)
+				@test successful_retcode(run!(ctest))
 			end
 		end
 		@testset "LinearCUDA" begin
@@ -103,6 +131,10 @@ const alldrivingfield_fns = getfield_functions.(alldrivingfields)
 			end
 			@testset "LinearTest (kxmax)" begin
 				ctest = makectest(sim1_kxmax, LinearTest(:kxmax, 10), lincuda)
+				@test successful_retcode(run!(ctest)) skip = skipcuda
+			end
+			@testset "PowerLawTest (dkx)" begin
+				ctest = makectest(sim2_dkx, PowerLawTest(:dkx,0.5), lincuda)
 				@test successful_retcode(run!(ctest)) skip = skipcuda
 			end
 		end

9fa71e4616b23f679c189aa780e3d0dd2a04ce5d
Wolfgang Hogger <wolfgang.hogger@ur.de>
2026-03-17 12:38:07 +0100

    Refactored composite field tests.

--- test/gpu/compositefield_gpu.jl
+++ test/gpu/compositefield_gpu.jl
@@ -9,26 +9,20 @@ using Test
 include(joinpath(@__DIR__, "..", "testutils.jl"))
 include(joinpath(@__DIR__, "..", "test_simulations.jl"))
 
-function test_composite_1d(sim::Simulation,fns,solver::DamysosSolver;
-    atol = 1e-10,
-    rtol = 1e-2)
-
-    run!(sim, fns, solver; saveplots = false, savedata = true, showinfo = false,
-        savepath = joinpath(testresults_dir(), sim.id))
-    return true
-end
-
-const sim_composite_1d = make_test_simulation_composite_1d(; id = "sim1d_composite_gpu")
+const sim_composite_1d_gpu = make_test_simulation_composite_1d(; id = "sim1d_composite_gpu")
 
 skipcuda = !(CUDA.functional())
 
 skipcuda && @warn "Skipping CUDA tests, CUDA.jl is not functional (mark as broken)."
 
 lincuda = skipcuda ? nothing : LinearCUDA(10_000, GPUVern7(), 1)
-const fns_1d_lincuda = skipcuda ? nothing : define_functions(sim_composite_1d, lincuda)
+const fns_1d_lincuda = skipcuda ? nothing : define_functions(sim_composite_1d_gpu, lincuda)
 
 @testset "CompositeField (GPU)" begin
     @testset "LinearCUDA" begin
-        @test test_composite_1d(sim_composite_1d, fns_1d_lincuda, lincuda) skip = skipcuda
+        @test !isempty(run!(sim_composite_1d_gpu, fns_1d_lincuda, lincuda;
+            showinfo = false, 
+            saveplots = false, 
+            savedata = false)) skip = skipcuda
     end
 end
--- test/slow/compositefield_cpu.jl
+++ test/slow/compositefield_cpu.jl
@@ -8,32 +8,28 @@ using Test
 include(joinpath(@__DIR__, "..", "testutils.jl"))
 include(joinpath(@__DIR__, "..", "test_simulations.jl"))
 
-function test_composite_1d(sim::Simulation,fns,solver::DamysosSolver;
-    atol = 1e-10,
-    rtol = 1e-2)
-
-    run!(sim, fns, solver; saveplots = false, savedata = true, showinfo = false,
-        savepath = joinpath(testresults_dir(), sim.id))
-    return true
-end
-
 function test_load_composite_1d(sim::Simulation, path::String; atol = 1e-10, rtol = 1e-8)
     loaded_sim = Simulation(path)
     return isapprox(sim, loaded_sim, atol = atol, rtol = rtol)
 end
 
-const sim_composite_1d = make_test_simulation_composite_1d(; id = "sim1d_composite_cpu")
+const sim_composite_1d_cpu = make_test_simulation_composite_1d(; id = "sim1d_composite_cpu")
 
 linchunked = LinearChunked()
-const fns_1d_linchunked = define_functions(sim_composite_1d, linchunked)
+const fns_1d_linchunked = define_functions(sim_composite_1d_cpu, linchunked)
 
 @testset "CompositeField" begin
+    savepath_cpu_composite_1d = joinpath(
+        testresults_dir(), sim_composite_1d_cpu.id, "cpu_composite_1d")
     @testset "LinearChunked" begin
-        @test test_composite_1d(sim_composite_1d, fns_1d_linchunked, linchunked)
+        @test !isempty(run!(sim_composite_1d_cpu, fns_1d_linchunked, linchunked;
+            showinfo = false, 
+            saveplots = false, 
+            savedata = true, 
+            savepath = savepath_cpu_composite_1d))
     end
     @testset "Loading from .hdf5" begin
         @test test_load_composite_1d(
-            sim_composite_1d,
-            joinpath(testresults_dir(), sim_composite_1d.id, "data.hdf5"))
+            sim_composite_1d_cpu, joinpath(savepath_cpu_composite_1d, "data.hdf5"))
     end
 end
